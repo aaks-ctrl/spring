9db69071e12a84e47e3622b62fdc0cfcf9b42fea
Aayush <58940228+aaks-ctrl@users.noreply.github.com>
2023-12-22 21:11:50 +0530

    springmvc ModelMap and String update

--- springmvc/src/main/java/com/aayush/spring/springmvc/controller/UserController.java
+++ springmvc/src/main/java/com/aayush/spring/springmvc/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.aayush.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.aayush.spring.springmvc.dto.User;
 
@@ -14,27 +14,22 @@ public class UserController {
 	User user = new User();
 
 	@RequestMapping("registrationPage")
-	public ModelAndView showRegPage() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("userReg");
-		return modelAndView;
+	public String showRegPage() {
+		return "userReg";
 	}
 
 	@RequestMapping("user")
-	public ModelAndView userDetails() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("result");
-		modelAndView.addObject("user", user);
-		return modelAndView;
+	public String userDetails(ModelMap model) {
+		model.addAttribute("user", user);
+		return "result";
 	}
 
 	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
-	public ModelAndView registerUser(@ModelAttribute("user") User user) {
+	public String registerUser(@ModelAttribute("user") User user, ModelMap model) {
 		System.out.println(user);
 		this.user = user;
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("result");
-		return modelAndView;
+		model.addAttribute("user", user);
+		return "result";
 	}
 
 }
