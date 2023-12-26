e43ba233cc4b65c3fd28694706d14e502df3b2f2
Aayush <58940228+aaks-ctrl@users.noreply.github.com>
2023-12-26 17:38:32 +0530

    Java configuration for spring MVC

---
+++ springmvc/src/main/java/com/aayush/spring/springmvc/controller/WebServletConfig.java
@@ -0,0 +1,23 @@
+package com.aayush.spring.springmvc.controller;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebServletConfig implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(SpringConfig.class);
+
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+	}
+
+}
